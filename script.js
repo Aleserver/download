49f86d713f5e63a886869c1acb1ce00754f74ffd
Aleserver <94232735+Aleserver@users.noreply.github.com>
2023-07-28 23:14:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function checkCode() {
   const inputCode = document.getElementById("loopeyline").value;
   if (inputCode === "5456" || inputCode === "2854983") {
-    window.location.href = "main";
+    window.location.href = "./main";
     return false; // Prevent form submission for now, as we're handling the redirection manually.
   } else {
     alert("Incorrect code. Please try again.");
